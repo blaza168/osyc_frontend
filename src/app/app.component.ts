b3948a239c4350f1b3ddf1beac339e8cb0f8d018
jan.blazek <JBlazekk@seznam.cz>
2019-02-15 17:07:59 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+class Post {
+  id: number;
+  content: string;
+  createdAt: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'osyc';
+
+  public posts: Post[];
+
+  public form: FormGroup;
+
+  constructor(private http: HttpClient) {
+    this.loadPosts();
+    this.form = this.buildForm();
+  }
+
+  public onSubmit(): void {
+    this.http.post('/post-app/post', this.form.value).subscribe(() => {
+      this.form.reset();
+      this.form.get('content').setErrors(null);
+      this.loadPosts();
+    });
+  }
+
+  private buildForm(): FormGroup {
+    return new FormGroup({
+      content: new FormControl(null, [Validators.required, Validators.maxLength(100)])
+    });
+  }
+
+  private loadPosts(): void {
+    this.http.get<Post[]>('/post-app/posts').subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
